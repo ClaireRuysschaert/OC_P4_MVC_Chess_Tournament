3ccb9540c6ce554202f7cd491419f8e73f32a17b
ClaireRuysschaert <netclaire@hotmail.fr>
2023-08-20 00:30:25 +0200

    feat: sort imports and black formatting. Add features to create matchs of Round 1.
    - Create empty matches of Round 1 with pair players
    - Get matches scores via user input
    - update Round instance link to the match (match_list)
    - update score of players instances
    - Check that all matches scores have been saved before creating an oter round.

--- views/main_menu.py
+++ views/main_menu.py
@@ -1,88 +1,103 @@
 import sys
 from pathlib import Path
+
 project_path = str(Path(__file__).parent.parent)
 sys.path.insert(0, project_path)
 
-from controllers.match import create_first_random_player_pairs, get_players_ine, create_match
-from controllers.tournament import create_new_tournament
+from controllers.match import update_matchs_score
 from controllers.round import create_new_round
-from utils.input_validation import get_integer_input, get_chess_national_identifier_input
-from views.match import get_matchs_score
-from views.player import display_player_creation_menu
-from views.tournament import get_tournament_info, check_all_players_created, create_add_players_to_tournament
+from controllers.tournament import create_new_tournament
 from models.tournament_model import Tournament
+from models.match_model import Match
+from utils.input_validation import (
+    get_chess_national_identifier_input,
+    get_integer_input,
+)
+from views.match import display_match_creation_menu, get_match_winner, play_matches_and_update_scores
+from views.player import display_player_creation_menu
+from views.tournament import create_add_players_to_tournament, get_tournament_info
+
 
 def main_menu_display() -> None:  # NOSONAR
     """First users entry point where all the functionalities are listed."""
-    
+
     while True:
-        user_input = get_integer_input("\nQuelle action voulez vous réaliser ?\n\n"
-                        "1 - Créer un tournoi\n"
-                        "2 - Charger et jouer un tournoi\n"
-                        "3 - Créer des joueurs\n"
-                        "4 - Afficher les rapports\n"
-                        "5 - Jouer un tournoi\n"
-                        "6 - Créer un match\n"
-                        "7 - Jouer un match\n"
-                        "0 - Quitter\n\n> ", 0, 7)
-    
+        user_input = get_integer_input(
+            "\nQuelle action voulez vous réaliser ?\n\n"
+            "1 - Créer un tournoi\n"
+            "2 - Charger et jouer un tournoi\n"
+            "3 - Créer des joueurs\n"
+            "4 - Afficher les rapports\n"
+            "5 - Jouer un tournoi\n"
+            "0 - Quitter\n\n> ",
+            0,
+            5,
+        )
+
         # 1 - Créer un tournoi
         if user_input == 1:
             # Récupérer les informations préliminaires du tournoi et les sauvegarder
             tournament_info = get_tournament_info()
             tournament_id = create_new_tournament(tournament_info)
             print(f"\nTournoi créé avec succès ! ID : {tournament_id}.")
-            print("Veuillez vous le noter si vous voulez charger les informations de ce tournoi.\n")
-            # Vérifier si les joueurs proposés par l'utilisateur sont à 
+            print(
+                "Veuillez vous le noter si vous voulez charger les informations de ce tournoi.\n"
+            )
+            # Vérifier si les joueurs proposés par l'utilisateur sont à
             # créer ou/et à ajouter au tournoi
             create_add_players_to_tournament(tournament_id)
-            
-        
+
         # 2 - Charger et jouer un tournoi
         elif user_input == 2:
-            #TODO: charger le tournoi
-            tournament_id = get_integer_input("Veuillez entrer l'ID du tournement à charger:\n", 1, 1000)
+            tournament_id = get_integer_input(
+                "Veuillez entrer l'ID du tournement à charger:\n", 1, 1000
+            )
             tournament = Tournament.get_tournament_id_from_db(tournament_id)
             current_round_number = Tournament.get_current_round_number(tournament)
-            create_new_round(tournament_id, current_round_number)
-        
+            # TODO: On ne veut pas forcément créer un round! Faire une condition, si le round n'existe pas, on le crée
+            # Si le Round n'a pas toutes les informations de scores, on reste sur l'existant!
+            round_id, player_pairs = create_new_round(
+                tournament_id, current_round_number
+            )
+            print(
+                f"\nVoici les concurrents des matchs du round {current_round_number+1} !"
+            )
+            display_match_creation_menu(round_id, player_pairs)
+            print(f"\nMaintenant, place aux matchs du round {current_round_number+1} !")
+            play_matches_and_update_scores()
+            
+            all_matches_played = Match.does_all_matches_have_been_played(int(round_id))
+            while not all_matches_played:
+                print("\nTous les matchs n'ont pas encore été joués. Veuillez renseigner les informations manquantes.")
+                play_matches_and_update_scores()
+                all_matches_played = Match.does_all_matches_have_been_played(int(round_id))
+            # TODO: le round id peut ne pas correspondre au round en cours (Round 1 mais id dans la db = 2)
+            print(f"\nTous les matchs du round {round_id} ont été joués !")
+            
+            # TODO: 7 - Créer le round suivant
+
+
         # 3 - Créer un joueur
         elif user_input == 3:
             print("\nVous avez choisi de créer un joueur.\n")
-            player_ine = get_chess_national_identifier_input("Veuillez entrer l'identifiant national d'échec du joueur à créer:\n>")
-            display_player_creation_menu(player_ine)     
-        
+            player_ine = get_chess_national_identifier_input(
+                "Veuillez entrer l'identifiant national d'échec du joueur à créer:\n>"
+            )
+            display_player_creation_menu(player_ine)
+
         # 4 - Afficher des rapports
-        elif user_input == 4: 
+        elif user_input == 4:
             print("to be continued...")
-            #afficher les résultats
-            #mettre à jour le classement 
-            
+            # afficher les résultats
+            # mettre à jour le classement
+
         # 5 - Jouer un tournoi
         elif user_input == 5:
             print("Créer un tournoi avec un controller.")
-        
-        # 6 - Créer le premier match
-        elif user_input == 6:
-            pair_players = create_first_random_player_pairs()
-            if check_all_players_created():
-                create_match(pair_players)
-                
-                print("Place aux matchs! Les joueurs suivant vont jouer l'un contre l'autre : \n")
-                for pair in pair_players:
-                    first_player = pair[0]["chess_national_identifier"]
-                    second_player = pair[1]["chess_national_identifier"]
-                    print(f"{first_player} versus {second_player}\n")
-            
-        # 7 - Jouer le premier round
-        elif user_input == 7:
-            pair_players = create_first_random_player_pairs()
-            players_ine = get_players_ine(pair_players)
-            scores_list = get_matchs_score(players_ine)
-            print(scores_list)
-            
+
         # Quitter
         else:
             quit()
-    
+
+
 main_menu_display()
