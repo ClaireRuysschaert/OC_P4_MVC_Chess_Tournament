aae434b417c0ca05f964c81e2e25b61bf89b0625
ClaireRuysschaert <netclaire@hotmail.fr>
2023-08-20 00:31:17 +0200

    feat: Add display match creation menu, get match winner functiun and play matches and update scores.

--- views/match.py
+++ views/match.py
@@ -1,27 +1,51 @@
-from typing import List, Tuple
+from controllers.match import create_match, update_matchs_score
+from models.match_model import Match
 from utils.input_validation import get_integer_input
-def get_matchs_score(players_ine: List[Tuple[str, str]]) -> List[Tuple[str, str]]:
-    """Get the match score by the user from pair players."""
-    
-    matchs_score: List[Tuple[str, float]] = []
-    for player_ine in players_ine:
-        #Ask to the user who have won the match for each pair
-        winner_or_tie = get_integer_input(f"Qui a gagné le match ? Si c'est : \n"
-        f" - {player_ine[0]}, tapez 1\n"
-        f" - {player_ine[1]}, tapez 2\n"
-        " - Un match nul, tapez 3.", 1, 3)
-        
-        # Save the score of each player in matchs_score list
-        if winner_or_tie == 1:
-            matchs_score.append((player_ine[0], 1))
-            matchs_score.append((player_ine[1], 0))
-        elif winner_or_tie == 2:
-            matchs_score.append((player_ine[0], 0))
-            matchs_score.append((player_ine[1], 1))
-        elif winner_or_tie == 3:
-            matchs_score.append((player_ine[0], 0.5))
-            matchs_score.append((player_ine[1], 0.5))
-        else:
-            raise ValueError("Please a number between 1 and 3.")
 
-    return matchs_score
+
+def display_match_creation_menu(round_id: str, player_pairs:list[list[str]]) -> None:
+    """Display the match creation menu."""
+    print(f"Les joueurs suivant vont jouer l'un contre l'autre :")
+    for i, pair in enumerate(player_pairs, start=1):
+                player1, player2 = pair
+                print(f"Match {i} : joueur 1, INE = {player1} versus joueur 2, INE = {player2}")
+                match_id = create_match(round_id, player_pairs[i-1])
+                print(f"Match créé avec succès ! ID : {match_id}.")
+
+def get_match_winner(match_id: int) -> int:
+    """
+    Display the match play menu.
+    Return match score that the user have entered.
+    1 means player one have won the match.
+    2 means player two have won the match.
+    3 means it's a tie.
+    """
+    match = Match.get_match_info_from_db(match_id)
+    match_winner = get_integer_input(f"Qui a gagné le match {match_id} ? Si c'est : \n"
+        f" - {match['player_one']}, tapez 1\n"
+        f" - {match['player_two']}, tapez 2\n"
+        " - Un match nul, tapez 3.\n", 1, 3)
+    return match_winner
+
+def play_matches_and_update_scores() -> None:
+    """
+    Play matches and update scores in a tournament.
+
+    This function allows the user to input match IDs, determine match winners,
+    and update match scores accordingly. It continues the process until the user
+    chooses to exit by entering match ID '0'.
+
+    Returns:
+        None
+    """
+    match_id = get_integer_input(
+                "Veuillez entrer l'ID du match à jouer:\n", 1, 1000
+            )
+    while match_id != 0:
+        match_winner = get_match_winner(match_id)
+        update_matchs_score(match_id, match_winner)
+        match_id = get_integer_input(
+            "Veuillez entrer l'ID du match suivant à jouer:\n Taper 0 pour quitter\n.",
+            0,
+            1000,
+        )
